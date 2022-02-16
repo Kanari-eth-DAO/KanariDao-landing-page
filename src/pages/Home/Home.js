bed6ea49df7a1246888c300cd48491c65c81e8a7
giorgosn <sdi1400127@di.uoa.gr>
2022-02-16 13:42:43 +0200

    remove rarity tab

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Hero from '../../components/Hero';
 import Learn from '../../components/Learn';
-import Rarity from '../../components/Rarity';
 import Generate from '../../components/Generate';
 import Roadmap from '../../components/Roadmap';
 import FAQ from '../../components/FAQ';
@@ -10,7 +9,6 @@ const Home = () => (
   <div className="home">
     <Hero />
     <Learn />
-    <Rarity />
     <Generate />
     <Roadmap />
     <FAQ />
